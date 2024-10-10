3d0a0163e969aab833f6344dac1afe9cdad7f751
jaakjuu1 <juuso.jaakkola@gmail.com>
2024-10-10 13:03:51 +0300

    build.format file

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 
 export default defineConfig({
+  build: {
+    format: 'file'
+  },
   output: 'server',
   adapter: node({
     mode: 'production',
